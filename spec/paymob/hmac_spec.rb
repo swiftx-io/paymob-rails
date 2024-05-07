0ddc908037f91ca84419128749d0940b2af4c953
Khaled Mustafa <khaled.mustafa@cis.asu.edu.eg>
2024-05-07 12:37:42 +0300

    Tests (#4)
    * Add pg gem
    
    * Setup rspec
    
    * Add first test case for engine version class
    
    * Edit database yml
    
    * Add debug gem
    
    * Add test for class variables
    
    * Add test for hmac calculations
    
    * convert hmac args to hash
    
    * Edit rails helpers
    
    * Add schema.rb
    
    * Add controller generator
    
    * Move vcr configuration to another file
    
    * Fix the error classes names
    
    * Add paymob vcr file
    
    * Add payment class tests
    
    * Lint
    
    * Move gem to gemspec
    
    * make any request outside vcr raise error
    
    * Add tests for accept api class
    
    * Lint
    
    * Lint

--- spec/paymob/hmac_spec.rb
+++ spec/paymob/hmac_spec.rb
@@ -8,18 +8,18 @@ RSpec.describe Hmac do
     before do
       Paymob.hmac_secret = 'DF42E0CDDDEABBC182E7297FC4C0206B'
     end
+
     context 'when request params hash matches the original hmac' do
-  
       it 'returns true' do
         expect(Hmac.matches_original?(HmacHelpers.response_example, HmacHelpers.correct_hmac)).to eq(true)
       end
     end
-  
+
     context 'when request params hash dose not matches the original hmac' do
       before do
         Paymob.hmac_secret = 'DF42E0CDDDEABBC182E7297FC4C0206B'
       end
-  
+
       it 'returns false' do
         expect(Hmac.matches_original?(HmacHelpers.response_example, HmacHelpers.wrong_hmac)).to eq(false)
       end
